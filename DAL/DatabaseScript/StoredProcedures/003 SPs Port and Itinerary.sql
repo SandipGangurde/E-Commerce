b3e2eed9c876984dc9e9b72bb3c54092baca80e8
Sandip Patil <sandip50sandip@gmail.com>
2024-04-10 08:30:02 +0530

    added categories sp

--- DAL/DatabaseScript/StoredProcedures/003 SPs Port and Itinerary.sql
+++ DAL/DatabaseScript/StoredProcedures/003 SPs Port and Itinerary.sql
@@ -42,3 +42,25 @@ SELECT CountryId,CountryCode,Country,[Status]
 FROM Countries WITH(NOLOCK) WHERE CountryId = @CountryId
 END
 ------------------------------------------------------------
+
+GO
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[SP_CategoriesByCategoryId]') AND type in (N'P', N'PC'))
+DROP PROCEDURE SP_CategoriesByCategoryId
+GO
+-- =============================================
+-- Author: <Author,,Sandip Patil>
+-- Create date: <Create Date,,10-Jan-2024>
+-- Schema Name: DBO
+-- Description: <Description,, Read timeZone based on TimeZoneId>
+-- History :
+-- =============================================
+
+CREATE PROCEDURE SP_CategoriesByCategoryId 
+@CategoryId AS BIGINT
+AS
+BEGIN
+SET NOCOUNT ON
+SELECT CategoryID,CategoryName,IsActive 
+FROM Categories WITH(NOLOCK) WHERE CategoryID = @CategoryId
+END
+------------------------------------------------------------
